554538fd35b8fcb68163e0dbcc5ba7c1c8993105
kelseydh <kelseyh@gmail.com>
2015-02-02 12:16:41 -0800

    set up endless scroll successfully

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,16 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+$(document).ready(function() {
+    if ($('.pagination').length) {
+      $(window).scroll(function() {
+        var url = $('.pagination .next_page').attr('href');
+        if (url && $(window).scrollTop() > $(document).height() - $(window).height() - 50) {
+          $('.pagination').text("Please Wait...");
+          return $.getScript(url);
+        }
+      });
+      return $(window).scroll();
+    }
+});
